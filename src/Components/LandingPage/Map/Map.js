dccc7cb8b0cb46ea093b3805927cf67dbddc2987
thegautamrohit <rgautam1997@gmail.com>
2024-04-14 15:42:00 +0530

    Map marker

--- src/Components/LandingPage/Map/Map.js
+++ src/Components/LandingPage/Map/Map.js
@@ -1,6 +1,17 @@
 import React, { useEffect, useRef } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon.src,
+  iconRetinaUrl: markerIcon2x.src,
+  shadowUrl: markerShadow.src,
+});
 
 const Map = ({ cities, onCityClick }) => {
   const mapRef = useRef(null);
@@ -22,7 +33,8 @@ const Map = ({ cities, onCityClick }) => {
         <Marker
           key={city.id}
           position={[city.lat, city.lng]}
-          onClick={() => handleCityClick(city)}
+          // onClick={() => handleCityClick(city)}
+          // icon={icon}
         >
           <Popup>{city.name}</Popup>
         </Marker>
